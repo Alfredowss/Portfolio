a7e7ef4c2298ea58af804aa1a8afbdb30c3cea51
Alfredwss <sick_freedylp@hotmail.com>
2025-12-05 00:34:00 -0600

    alfred(spike): created base for welcome page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,17 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+	server: {
+		host: true,
+		port: 4321
+	},
+	vite: {
+		plugins: [ tailwindcss() ]
+	},
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
 });
